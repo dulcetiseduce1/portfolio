d93b13cffabd31b86b19fd2bc94394c509ae7453
dulcetiseduce1 <mariusdulce@tuta.io>
2022-12-02 13:12:42 +0100

    progetti

--- pages/index.vue
+++ pages/index.vue
@@ -5,24 +5,29 @@
       background-image: url('https://images.unsplash.com/photo-1619252584172-a83a949b6efd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80');
     "
   >
-    <div>
+    <div class="grid">
       <div class="p-6 text-2xl font-semibold md:text-4xl font-Syne">
         <p class="inline underline">Informazioni</p>
         <NuxtLink to="progetti">
-          <p class="inline pl-6">Progetti</p>
+          <p class="inline pl-4">Progetti</p>
         </NuxtLink>
       </div>
+
+
       <div
-        class="p-6 text-6xl md:text-[8rem] lg:text-[12rem] leading-none font-Syne font-bold align-bottom absolute bottom-0"
+        class="p-6 text-2xl font-semibold leading-none md:text-4xl font-Syne"
+      >
+        <p class="">Coltivo la passione</p>
+        <p class="">per il design e la tecnologia</p>
+        <p class="">utilizzando forme e codice</p>
+        <p class="">per realizzare le mie visioni.</p>
+      </div>
+      <div
+        class="p-6 text-6xl md:text-[8rem] lg:text-[12rem] leading-none font-Syne font-bold"
       >
         <h1 class="">Marius</h1>
         <h1 class="">Dulce</h1>
       </div>
     </div>
-    <div class="p-6 pr-14 pt-[10rem] text-2xl md:text-4xl leading-none font-Syne font-semibold align-bottom absolute top-0 right-0">
-        <p class="">Coltivo la passione</p> <p class="">per il design e la tecnologia</p>
-        <p class="">utilizzando forme e codice</p>
-        <p class="">per realizzare le mie visioni.</p>
-      </div>
   </div>
 </template>
--- pages/progetti.vue
+++ pages/progetti.vue
@@ -5,12 +5,14 @@
       background-image: url('https://images.unsplash.com/photo-1619252584172-a83a949b6efd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80');
     "
   >
-    <div class="p-6 text-2xl font-semibold md:text-4xl font-Syne">
-      <NuxtLink to="/">
-        <p class="inline">Informazioni</p> 
-    </NuxtLink>
-      <p class="inline pl-6 underline">Progetti</p>
-    </div>
+  <div class="grid">
+      <div class="p-6 text-2xl font-semibold md:text-4xl font-Syne">
+        <NuxtLink to="/">
+        <p class="inline ">Informazioni</p>
+        </NuxtLink>
+          <p class="inline pl-4 underline">Progetti</p>
+        
+      </div>
     <div class="grid text-4xl font-bold pt-9 font-Syne place-items-center">
       <NuxtLink to="https://social-test-podas.vercel.app/">
       <p class="">Podas</p>
@@ -19,5 +21,6 @@
       <p class="">Github</p>
       </NuxtLink>
     </div>
+  </div>
   </div>
 </template>
